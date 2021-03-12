b5cf747958a9021d0f86a5bf62d0d0d97f51fd56
Cesar Romagnolo <cesar.romagnolo@hotmail.com>
2021-03-12 14:47:30 -0300

    Aula 86 - Criando Estilos

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import {Title} from './styles';
+
 export default function Main(){
     return(
-        <div>
-            <h1>Main</h1>
+        <div>   
+            <Title>
+                Main
+            </Title>
         </div>
     );
 }
