370f1adc1be60f45f9f7fe8c7bdb6d51d61b0aaf
Cesar Romagnolo <cesar.romagnolo@hotmail.com>
2021-03-14 10:22:58 -0300

    Aula 90 - Listando Repositorios

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,50 +1,115 @@
-import styled from 'styled-components';
+import styled, {keyframes, css} from 'styled-components';
 
 export const Container = styled.div`
-    max-width: 700px;
-    background-color: #FFF;
-    border-radius: 4px;
-    box-shadow: 0 0 20px rgb(0 0 0 / 20%);
-    padding: 30px;
-    margin: 80px auto;
+  max-width: 700px;
+  background: #FFF;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0,0,0, 0.2);
+  padding: 30px;
+  margin: 80px auto;
 
-    h1{
-        font-size: 20px;
-        display: flex;
-        align-items: center;
-        flex-direction: row;
-
-        svg{
-            margin-right: 10px;
-        }
-
+  h1{
+    font-size: 20px;
+    display:flex;
+    align-items: center;
+    flex-direction:row;
+    
+    svg{
+      margin-right: 10px;
     }
+
+  }
+
+
 `;
 
 export const Form = styled.form`
-    margin-top: 30px;
-    display:flex;
-    flex-direction: row;
+  margin-top: 30px;
+  display:flex;
+  flex-direction: row;
 
-    input{
-        flex:1;
-        border: 1px solid #DDD;
-        padding: 10px 15px;
-        border-radius: 4px;
-        font-size: 17px;
-    }
+  input{
+    flex:1;
+    border: 1px solid #DDD;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 17px;
+  }
+
 `;
 
-export const SubmitButton = styled.button.attrs({
-    type: 'submit'
-})`
-    background: #0D2636;
-    border: 0;
-    border-raidius: 4px;
-    margin-left: 10px;
-    padding: 0 15px;
-    display: flex;
-    justify-content: center;
-    align-items: center
+//Criando animcação do botao
+const animate = keyframes`
+  from{
+    transform: rotate(0deg);
+  }
 
+  to{
+    transform: rotate(360deg);
+  }
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
+  type: 'submit',
+  disabled: props.loading,
+}))`
+  background:#0D2636;
+  border: 0;
+  border-radius: 4px;
+  margin-left: 10px;
+  padding: 0 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+
+  &[disabled]{
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
+
+
+  ${props => props.loading &&
+    css`
+      svg{
+        animation: ${animate} 2s linear infinite;
+      }
+    `
+  }
+
+
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 20px;
+
+  li{
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    
+    & + li{
+      border-top: 1px solid #DDD;
+    }
+
+    a{
+      color: #0D2636;
+      text-decoration: none;
+    }
+  }
+`;
+
+export const DeleteButton = styled.button.attrs({
+  type:'button'
+})`
+  margin-left: 6px;
+  background: transparent;
+  color: 0D2636;
+  border: 0px;
+  padding: 8px 7px;
+  outline: 0;
+  border-radius: 4px;
 `;
