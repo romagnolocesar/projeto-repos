0da4bc7443e78ac12300535a228a8fcf6e4ed8af
Cesar Romagnolo <cesar.romagnolo@hotmail.com>
2021-03-15 17:24:52 -0300

    Aula 94- Exibir detalhes

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,6 @@
 import React, {useState, useCallback, useEffect} from 'react';
 import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
 import {Container, Form, SubmitButton, List, DeleteButton} from './styles';
 
 import api from '../../services/api';
@@ -107,9 +108,9 @@ export default function Main(){
               </DeleteButton>
               {repo.name}
             </span>
-            <a href="">
+            <Link to={`/repositorio/${encodeURIComponent(repo.name)}/`}>
               <FaBars size={20}/>
-            </a>
+            </Link>
           </li>    
         ))}        
       </List>        
